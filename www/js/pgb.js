77dee1b0039177309c7ee83546027f015b0a2761
s-116-26 <s-116-26@gumiforest.ae.krakow.pl>
2017-03-04 17:29:24 +0100

    fix: add accelerator

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -4,6 +4,7 @@ function init() {
 
 function onDeviceReady() {
 	navigator.notification.beep(1);
+        navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
 }
 
 function deviceInfo() {
@@ -42,4 +43,15 @@ function checkConnection() {
     alert('Connection type: ' + states[networkState]);
 }
  
-checkConnection();
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+var options = { frequency: 3000 };  // Update every 3 seconds
