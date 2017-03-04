443efdd7f47e5900384bde18e50b51e56590dc8c
s-116-26 <s-116-26@gumiforest.ae.krakow.pl>
2017-03-04 17:17:09 +0100

    business card app

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -8,14 +8,19 @@ function onDeviceReady() {
 
 function deviceInfo() {
 
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
+//	info =  'Hi, I am your smartphone :-)' + '\n' +
+//			'=====' + '\n' +
+//			'Device Name    : '     + device.name     + '\n' + 
+//			'Device Cordova : '  + device.cordova + '\n' + 
+//			'Device Platform: ' + device.platform + '\n' + 
+//			'Device UUID    : '     + device.uuid     + '\n' + 
+//			'Device Model   : '    + device.model     + '\n' + 
+//			'Device Version : '  + device.version  + '\n';
+        info =  'Hi, its my business card' + '\n' +
+		'Name    : Mateusz' + '\n' +
+	        'Surname : Jaskula' + '\n' +
+	        'email   : jaskula92@gmail.com' + '\n' +
+	        'Field of study: Applied Informatics' + '\n';	
 
 	navigator.notification.alert(info);
 	
